fbfcf15a0f8038575b119b8718242de7e65dbec9
xinchaoduyanh <duyanh19122k3@gmail.com>
2025-05-18 17:57:12 +0700

    1

--- apps/mobile-app/config/env.ts
+++ apps/mobile-app/config/env.ts
@@ -1,4 +1,8 @@
 export const config = {
   //Bật máy chủ backend trên máy cá nhân + IPCONFIG rồi thay đổi IP bên dưới
+<<<<<<< Updated upstream
   API_URL: "http://192.168.137.37:3000", // Đúng với IP local của bạn
+=======
+  API_URL: "http://192.168.137.133:3000", // Đúng với IP local của bạn
+>>>>>>> Stashed changes
 };
