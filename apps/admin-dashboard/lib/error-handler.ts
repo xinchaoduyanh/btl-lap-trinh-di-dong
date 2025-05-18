e202ac693b39fcb1e8f6ebae0d603d852aabb1ce
foq2 <hungphongb@gmail.com>
2025-05-18 13:40:25 +0700

    [Admin-dashboard] fix employee UI

--- apps/admin-dashboard/lib/error-handler.ts
+++ apps/admin-dashboard/lib/error-handler.ts
@@ -1,12 +1,17 @@
 export function getApiErrorMessage(error: any): string {
   // Axios error
-  if (error?.response?.data?.message) {
-    return error.response.data.message;
+  if (error?.response?.data) {
+    if (error.response.data.error && error.response.data.message) {
+      return `${error.response.data.error}: ${error.response.data.message}`
+    }
+    if (error.response.data.message) {
+      return error.response.data.message
+    }
   }
-  // Fetch error (nếu dùng fetch)
+  // Fetch error
   if (error?.message) {
-    return error.message;
+    return error.message
   }
   // Fallback
-  return "Đã có lỗi xảy ra, vui lòng thử lại!";
+  return 'Đã có lỗi xảy ra, vui lòng thử lại!'
 }
