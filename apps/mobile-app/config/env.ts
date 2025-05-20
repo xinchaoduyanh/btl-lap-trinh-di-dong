d1dc39c41e17e6d22563ac25161838104a9c50a5
foq2 <hungphongb@gmail.com>
2025-05-20 17:24:27 +0700

    [mobile-app] update env

--- apps/mobile-app/config/env.ts
+++ apps/mobile-app/config/env.ts
@@ -1,4 +1,3 @@
 export const config = {
-  //Bật máy chủ backend trên máy cá nhân + IPCONFIG rồi thay đổi IP bên dưới
-  API_URL: 'http://10.0.2.2:3000', // Thay đổi IP này thành IP hiện tại của máy chủ backend
+  API_URL: 'http://192.168.137.32:3000', // Thay đổi IP này thành IP hiện tại của máy chủ backend
 }
